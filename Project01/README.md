440ec9df1cca53e15edbeb69755d21ad9aa8b597
zaidia10 <zaidia10@mcmaster.ca>
2019-02-27 02:19:20 +0000

    did part two of the assignment

--- Project01/README.md
+++ Project01/README.md
@@ -2,8 +2,16 @@ Features Implemented
 -------------------------------
 1. Script Input
 	When the project script is executed, the user is prompted to enter their name
-	and then asked if they would like to see the TODO Log, which is the only other
-	feature in the script
+	and then asked if they would like to see the TODO Log. If they do not say yes,
+	they are asked if they would like to see the File Type Count.
 2. TODO Log
 	When requested, the TODO log searches through every file within the CS1XA3 directory
 	and returns, line by line, every line with the phrase '#TODO' in it.
+3. File Type Count
+	When requested, the File Type Count searches every file name within the CS1XA3 directory
+	and returns the count of certain extensions found (HTML, Bash, CSS, Python, Haskell,
+	JavaScript).
+4. Name Search
+	Since the user has already typed in their name, upon request, this features searches
+	every file within the CS1XA3 directory and returns, line by line, every line with the
+	user's name that they previously entered.
--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,8 +1,8 @@
 echo "Hey! What's your name?"
 read name
 echo "Right on, dude. Do you want to see the TODO Log?"
-read answer
-if [ $answer == "Yes" ] || [ $answer == "yes" ];
+read answer1
+if [ $answer1 == "Yes" ] || [ $answer1 == "yes" ];
 then
 	if [ -f ~/CS1XA3/Project01/todo.log ];
 	then
@@ -14,6 +14,27 @@ then
 		linesTodo=$(grep -Rn '#TODO' /home/zaidia10/CS1XA3)
 		echo "$linesTodo" > "todo.log"
 	fi
+fi
+
+echo "Do you want to see the File Type Count?"
+read answer2
+if [ $answer2 == "Yes" ] || [ $answer2 == "yes" ];
+then
+	htmlCount="$(find /home/zaidia10/CS1XA3 -name '*.html' | wc -l)"
+	jsCount="$(find /home/zaidia10/CS1XA3 -name '*.js' | wc -l)"
+	cssCount="$(find /home/zaidia10/CS1XA3 -name '*.css' | wc -l)"
+	pyCount="$(find /home/zaidia10/CS1XA3 -name '*.py' | wc -l)"
+	hsCount="$(find /home/zaidia10/CS1XA3 -name '*.hs' | wc -l)"
+	shCount="$(find /home/zaidia10/CS1XA3 -name '*.sh' | wc -l)"
+	echo "HTML: " "$htmlCount" " JavaScript: " "$jsCount" " CSS: " "$cssCount" " Python: " "$pyCount" " Haskell: " "$pyCount" " Bash: " "$shCount"
+fi
+
+echo "Do you want to see where your name is mentioned?"
+read answer3
+if [ $answer3 == "Yes" ] || [ $answer3 == "yes" ];
+then
+	nameMentions=$(grep -Rn "$name" /home/zaidia10/CS1XA3)
+	echo "$nameMentions"
 else
-	echo "No worries, dude. Please execute this again"
+	echo "Dang, dude. That's all I got. Execute the file again to get the options again"
 fi
